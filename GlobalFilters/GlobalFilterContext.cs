274501fe01a85e1bf07fcd59cadc4eeb40389ca2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-01-15 17:02:55 +0100

    Dodanie globalnych filtrów

---
+++ GlobalFilters/GlobalFilterContext.cs
@@ -0,0 +1,26 @@
+﻿using GlobalFilters.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalFilters
+{
+    internal class GlobalFilterContext : DbContext
+    {
+        public GlobalFilterContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasQueryFilter(p => !p.IsRemoved);
+        }
+
+
+    }
+}
--- GlobalFilters/Program.cs
+++ GlobalFilters/Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Bogus;
+using GlobalFilters;
+using GlobalFilters.Models;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+string connectionString = @"Server=(localdb)\mssqllocaldb;Database=GlobalFiltersDb";
+
+// Install-Package Microsoft.EntityFrameworkCore.SqlServer
+var options = new DbContextOptionsBuilder<GlobalFilterContext>()
+    .UseSqlServer(connectionString)
+    .Options;
+
+using var context = new GlobalFilterContext(options);
+
+context.Database.EnsureDeleted();
+context.Database.EnsureCreated();
+
+var customers = new Faker<Customer>()
+    .RuleFor(p => p.FirstName, f => f.Person.FirstName)
+    .RuleFor(p => p.LastName, f => f.Person.LastName)
+    .RuleFor(p => p.IsRemoved, f => f.Random.Bool(0.2f))
+    .Generate(100);
+
+context.Customers.AddRange(customers);
+context.SaveChanges();
+
+var activeCustomers = context.Customers.ToList();
+
+
+
+foreach (var item in activeCustomers)
+{
+    Console.WriteLine(item);
+}
+
+var allCustomers = context.Customers.IgnoreQueryFilters().ToList();
+
+foreach (var item in allCustomers)
+{
+    Console.WriteLine(item);
+}
+
+
