c2b1eece6d27220879cdbfea58e98c9c94f5612d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-01-15 09:59:26 +0100

    Fix

--- Conventions/Program.cs
+++ Conventions/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Hello, Conventions!");
 
-string connectionString = "Server=(localdb)\mssqllocaldb;Database=OrdersDb";
+string connectionString = @"Server=(localdb)\mssqllocaldb;Database=OrdersDb";
 
 var options = new DbContextOptionsBuilder<OrdersContext>()
     .UseSqlServer(connectionString)
