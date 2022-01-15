74819abbbc053db834db1ac9fe6132310598a430
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-01-15 10:37:11 +0100

    Dodanie przykładu

---
+++ Migrations/OrderContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Migrations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrations
+{
+    public class IdentityDbContext : DbContext
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+
+    public class CustomersContext : DbContext
+    {
+        public CustomersContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- Migrations/Program.cs
+++ Migrations/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.EntityFrameworkCore;
+using Migrations;
+
+Console.WriteLine("Hello, Migrations!");
+
+string connectionString = @"Server=(localdb)\mssqllocaldb;Database=OrdersDb";
+
+// Install-Package Microsoft.EntityFrameworkCore.SqlServer
+
+var options = new DbContextOptionsBuilder<CustomersContext>()
+    .UseSqlServer(connectionString)
+    .Options;
+
+using var context = new CustomersContext(options);
