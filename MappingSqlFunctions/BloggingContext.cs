a478d83f864aecb05e67e6fe7f07e33f20317b27
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-01-16 18:23:23 +0100

    Mapowanie własnej funkcji dyskretnej SQL

---
+++ MappingSqlFunctions/BloggingContext.cs
@@ -0,0 +1,45 @@
+﻿using MappingSqlFunctions.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MappingSqlFunctions
+{
+    internal class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs  { get; set; }
+
+        public BloggingContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public int ActivePostCountForBlog(int blogId) => throw new NotSupportedException();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()              
+                .Property(p => p.Id).HasColumnName("BlogId");
+
+            modelBuilder.Entity<Post>()
+                .ToTable("Posts")
+                .Property(p => p.Id).HasColumnName("PostId");
+                
+
+            modelBuilder.Entity<Comment>()
+                .ToTable("Comments")
+                .Property(p => p.Id).HasColumnName("CommentId");
+
+
+            modelBuilder.HasDbFunction(typeof(BloggingContext)
+                .GetMethod(nameof(ActivePostCountForBlog), new[] { typeof(int) }))
+                .HasName("CommentedPostCountForBlog");
+        }
+
+       
+
+
+    }
+}
--- MappingSqlFunctions/Program.cs
+++ MappingSqlFunctions/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bogus;
+using MappingSqlFunctions;
+using MappingSqlFunctions.Models;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, MappingSqlFunctions!");
+
+
+string connectionString = @"Server=(localdb)\mssqllocaldb;Database=MappingSqlFunctionsDb";
+
+// Install-Package Microsoft.EntityFrameworkCore.SqlServer
+var options = new DbContextOptionsBuilder<BloggingContext>()
+    .UseSqlServer(connectionString)
+    .Options;
+
+using var context = new BloggingContext(options);
+
+
+
+if (context.Database.EnsureCreated())
+{
+
+    context.Database.ExecuteSqlRaw(@"
+           CREATE FUNCTION dbo.CommentedPostCountForBlog(@id int)
+            RETURNS int
+            AS
+            BEGIN
+                RETURN (SELECT COUNT(*)
+                    FROM [Posts] AS [p]
+                    WHERE ([p].[BlogId] = @id) AND ((
+                        SELECT COUNT(*)
+                        FROM [Comments] AS [c]
+                        WHERE [p].[PostId] = [c].[PostId]) > 0));
+            END
+");
+
+    var commentFaker = new Faker<Comment>()
+        .RuleFor(p => p.Content, f => f.Lorem.Sentences(3))
+        .RuleFor(p=>p.Author, f=>f.Person.FullName);
+
+    var postFaker = new Faker<Post>()
+        .RuleFor(p => p.Content, f => f.Lorem.Text())
+        .RuleFor(p => p.Comments, f => commentFaker.GenerateBetween(0, 10));
+
+
+    var blogFaker = new Faker<Blog>()
+        .RuleFor(p=>p.Title, f=>f.Lorem.Sentence())
+        .RuleFor(p => p.Posts, f => postFaker.Generate(20));
+
+
+    var blogs = blogFaker.Generate(50);
+
+
+
+
+    context.Set<Blog>().AddRange(blogs);
+    context.SaveChanges();
+}
+
+
+
+var query1 = from b in context.Blogs
+             where context.ActivePostCountForBlog(b.Id) > 10
+             select b;
+
+
+Console.WriteLine(query1);
+
+
+
+
+
+
