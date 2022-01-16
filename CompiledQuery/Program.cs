183753e058efd298b4905f75ca60e0e6f9fda52e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-01-16 17:58:13 +0100

    Link How Entity Framework translates LINQ all the way to SQL - Dotnetos Conference 2019

--- CompiledQuery/Program.cs
+++ CompiledQuery/Program.cs
@@ -42,4 +42,7 @@ Console.WriteLine(foundVehicle2);
 // dotnet ef dbcontext optimize
 // https://docs.microsoft.com/pl-pl/ef/core/cli/dotnet#dotnet-ef-dbcontext-optimize
 
+// How Entity Framework translates LINQ all the way to SQL - Dotnetos Conference 2019
+https://www.youtube.com/watch?v=r69ZxXgOIK4
+
 
