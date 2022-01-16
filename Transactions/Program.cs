74a750e5a112dc5361583e31901ae1d2ec42da27
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-01-16 12:56:41 +0100

    Dodanie przykładu z transakcjami

--- Transactions/Program.cs
+++ Transactions/Program.cs
@@ -1,9 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Transactions;
 using Transactions.Models;
 
 Console.WriteLine("Hello, Transactions!");
 
+string connectionString = @"Server=(localdb)\mssqllocaldb;Database=TransactionsDb";
+
+// Install-Package Microsoft.EntityFrameworkCore.SqlServer
+var options = new DbContextOptionsBuilder<TransactionContext>()
+    .UseSqlServer(connectionString)
+    .Options;
+
+using var context = new TransactionContext(options);
+
+if (context.Database.EnsureCreated())
+{
+    var accounts = GenerateAccounts();
+    context.Accounts.AddRange(accounts);
+    context.SaveChanges();
+}
+
 // TODO: safe transfer money
+
+decimal amount = 100;
+
+var sender = context.Accounts.Single(a => a.AccountNumber == "1111");
+sender.Balance -= amount;
+
+context.SaveChanges();
+
+var recipient = context.Accounts.Single(a => a.AccountNumber == "2222");
+recipient.Balance += amount;
+
+context.SaveChanges();
 
 
 static void Display(IEnumerable<Account> accounts)
---
+++ Transactions/TransactionContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transactions.Models;
+
+namespace Transactions
+{
+    internal class TransactionContext : DbContext
+    {
+        public TransactionContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+
+    }
+}
