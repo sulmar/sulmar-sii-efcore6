12caeddb6ef7aac785cfa5b257e8e26644582e0e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-01-16 17:58:04 +0100

    Refaktoring

---
+++ QueryUpdateMapping/PostConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QueryUpdateMapping.Models;
+
+namespace QueryUpdateMapping
+{
+    public class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder
+                .ToTable("Posts")
+                // .ToView("PostsView")
+                .ToSqlQuery(@"SELECT TOP(5) * FROM Posts ORDER BY CreateDate desc")
+                .ToFunction("")
+                ;
+        }
+    }
+}
--- QueryUpdateMapping/PostContext.cs
+++ QueryUpdateMapping/PostContext.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using QueryUpdateMapping.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,18 +21,5 @@ namespace QueryUpdateMapping
         }
 
 
-    }
-
-    public class PostConfiguration : IEntityTypeConfiguration<Post>
-    {
-        public void Configure(EntityTypeBuilder<Post> builder)
-        {
-            builder
-                .ToTable("Posts")
-                // .ToView("PostsView")
-                .ToSqlQuery(@"SELECT TOP(5) * FROM Posts ORDER BY CreateDate desc")
-               // .ToFunction("")
-                ;
-        }
     }
 }
